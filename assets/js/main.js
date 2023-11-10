772ac0b3b7aa9a2b2e691ac018bb47f862a5d366
Miguel <miguelmds925@gmail.com>
2023-11-10 16:07:34 -0300

    Aprendendo a mexer com JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,17 @@ const menu = document.querySelector(".menu")
 
 
 menuResponsive.addEventListener("click", () => {
-  
-
   if(menu.classList.contains("active")) {
     menu.classList.remove("active")
   }else {
     menu.classList.add("active")
   }
-
-
 })
+
+function potions() {
+  fetch("potions.json").then((response) => {
+    response.json().then((potions) => {
+      console.log(potions);
+    })
+  })
+}
