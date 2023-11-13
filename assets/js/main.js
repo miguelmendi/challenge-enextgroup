e3dca3eb941649c88b5379f7d79428ed89dde7ba
Miguel <miguelmds925@gmail.com>
2023-11-13 20:22:19 -0300

    Tentando fazer uma função para retornar um id

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,14 @@
 import JSON from './potions.json' assert { type: "json" };
 console.log(JSON)
 
+const potionsDiv = document.querySelector(".main-img")
 
+potionsDiv.addEventListener("click ", ()=> {
+  return IdleDeadline
+})
 
 const menuResponsive = document.querySelector("#menu-responsive")
 const menu = document.querySelector(".menu")
-
 
 menuResponsive.addEventListener("click", () => {
     menu.classList.toggle("active")
