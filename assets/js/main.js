abc6e6a833740d29df687955c782ca351f1487d3
Miguel <miguelmds925@gmail.com>
2023-11-15 16:18:05 -0300

    Tentando fazer o switc funcionar

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,41 @@ console.log(JSON)
 
 let potionsDiv = document.querySelectorAll(".main-img")
 
-function getId()  {
+ function getId()  {
   for(var el of potionsDiv){
     el.addEventListener('click', function(){
-      console.log(this.id);
-      return this.id
+      const id = this.id
+      console.log(id)
+      return id
     });
   }   
 }
 
+
+
+getId()
+ 
+
+const aside = document.querySelector(".aside")
+const asideImg = document.querySelector("aside-img")
+
 function addInfos() {
   switch (getId()) {
-    case
+    case 1:
+      asideImg.content = JSON.potions[1].image
+    case 2:
+
+    case 3:
+
+    case 4:
+
+    case 5:
+
+    case 6:
   }
 
 }
-
-  
+addInfos()
 getId()
 
 
